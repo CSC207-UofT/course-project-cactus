2cc31b66d0f1682dd9af163c066f59592e416d7c
calebxcaleb <calebxcaleb@gmail.com>
2021-10-15 12:20:28 -0400

    Implemented and fixed test cases for GroceryListSystem

--- src/test/java/com/cactus/systems/GroceryListSystemTest.java
+++ src/test/java/com/cactus/systems/GroceryListSystemTest.java
@@ -1,131 +1,138 @@
 package com.cactus.systems;
 
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
-class GroceryListSystemTest {
-    GroceryListSystem groceryListSystem;
+public class GroceryListSystemTest {
+    static GroceryListSystem groceryListSystem;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         groceryListSystem = new GroceryListSystem();
     }
 
     @Test
-    void testCreateUserDefault() {
-        assertTrue(this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password"));
+    public void testCreateUserDefault() {
+        assertTrue(groceryListSystem.createUser("Caleb", "calebxcaleb", "password"));
     }
 
     @Test
-    void testCreateUserEmpty() {
-        assertTrue(this.groceryListSystem.createUser("", "", ""));
+    public void testCreateUserEmpty() {
+        assertTrue(groceryListSystem.createUser("", "", ""));
     }
 
     @Test
-    void testCreateUserSameUsername() {
+    public void testCreateUserSameUsername() {
         this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        assertFalse(this.groceryListSystem.createUser("Sadler", "calebxcaleb", "123"));
+        assertFalse(groceryListSystem.createUser("Sadler", "calebxcaleb", "123"));
     }
 
     @Test
-    void testDeleteUserDefault() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        assertTrue(this.groceryListSystem.deleteUser("calebxcaleb", "password"));
+    public void testDeleteUserDefault() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        assertTrue(groceryListSystem.deleteUser("calebxcaleb", "password"));
     }
 
     @Test
-    void testDeleteUserDNE() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        assertFalse(this.groceryListSystem.deleteUser("human", "123"));
+    public void testDeleteUserDNE() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        assertFalse(groceryListSystem.deleteUser("human", "123"));
     }
 
     @Test
-    void testLoginDefault() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        assertTrue(this.groceryListSystem.login("calebxcaleb", "password"));
+    public void testLoginDefault() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        assertTrue(groceryListSystem.login("calebxcaleb", "password"));
     }
 
     @Test
-    void testLoginWrongUsername() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        assertFalse(this.groceryListSystem.login("Caleb", "password"));
+    public void testLoginWrongUsername() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        assertFalse(groceryListSystem.login("Caleb", "password"));
     }
 
     @Test
-    void testLoginWrongPassword() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        assertFalse(this.groceryListSystem.login("calebxcaleb", "123"));
+    public void testLoginWrongPassword() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        assertFalse(groceryListSystem.login("calebxcaleb", "123"));
     }
 
     @Test
-    void testLogoutDefault() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        this.groceryListSystem.logout();
+    public void testLogoutDefault() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        groceryListSystem.logout();
         // TODO figure out a test for this method
         fail();
     }
 
     @Test
-    void testNewGroceryListDefault() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        assertTrue(this.groceryListSystem.newGroceryList("List 1"));
+    public void testNewGroceryListDefault() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        assertTrue(groceryListSystem.newGroceryList("List 1"));
     }
 
     @Test
-    void testNewGroceryListSameName() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        this.groceryListSystem.newGroceryList("List 1");
-        assertFalse(this.groceryListSystem.newGroceryList("List 1"));
+    public void testNewGroceryListSameName() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        groceryListSystem.newGroceryList("List 1");
+        assertFalse(groceryListSystem.newGroceryList("List 1"));
+        // TODO: Do not allow same list names
     }
 
     @Test
-    void testNewItemDefault() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        this.groceryListSystem.newGroceryList("List 1");
-        assertTrue(this.groceryListSystem.newItem("Item 1"));
+    public void testNewItemDefault() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        groceryListSystem.newGroceryList("List 1");
+        assertTrue(groceryListSystem.newItem("Item 1"));
+        // TODO: Do not allow same item names
     }
 
     @Test
-    void testNewItemSameName() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        this.groceryListSystem.newGroceryList("List 1");
-        this.groceryListSystem.newItem("Item 1");
-        assertFalse(this.groceryListSystem.newItem("Item 1"));
+    public void testNewItemSameName() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        groceryListSystem.newGroceryList("List 1");
+        groceryListSystem.newItem("Item 1");
+        assertFalse(groceryListSystem.newItem("Item 1"));
     }
 
     @Test
-    void testGetGroceryListNames() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        this.groceryListSystem.newGroceryList("List 1");
-        this.groceryListSystem.newGroceryList("List 2");
-        this.groceryListSystem.newGroceryList("List 3");
-        this.groceryListSystem.newGroceryList("List 4");
+    public void testGetGroceryListNames() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        groceryListSystem.newGroceryList("List 1");
+        groceryListSystem.newGroceryList("List 2");
+        groceryListSystem.newGroceryList("List 3");
+        groceryListSystem.newGroceryList("List 4");
 
         String[] expected = {"List 1", "List 2", "List 3", "List 4"};
-        HashMap<String, Long> actualSet = this.groceryListSystem.getGroceryListNames();
+        HashMap<String, Long> actualSet = groceryListSystem.getGroceryListNames();
         String[] actual = actualSet.keySet().toArray(new String[4]);
+        Arrays.sort(actual);
 
-        assertEquals(expected, actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
-    void testGetGroceryItemNames() {
-        this.groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
-        this.groceryListSystem.newGroceryList("List 1");
-        this.groceryListSystem.newItem("Item 1");
-        this.groceryListSystem.newItem("Item 2");
-        this.groceryListSystem.newItem("Item 3");
-        this.groceryListSystem.newItem("Item 4");
+    public void testGetGroceryItemNames() {
+        groceryListSystem.createUser("Caleb", "calebxcaleb", "password");
+        groceryListSystem.newGroceryList("List 1");
+        groceryListSystem.newItem("Item 1");
+        groceryListSystem.newItem("Item 2");
+        groceryListSystem.newItem("Item 3");
+        groceryListSystem.newItem("Item 4");
 
         String[] expected = {"Item 1", "Item 2", "Item 3", "Item 4"};
-        String[] actual = this.groceryListSystem.getGroceryItemNames().toArray(new String[4]);
+        String[] actual = groceryListSystem.getGroceryItemNames().toArray(new String[4]);
+        Arrays.sort(actual);
 
-        assertEquals(expected, actual);
+        assertArrayEquals(expected, actual);
     }
 }
