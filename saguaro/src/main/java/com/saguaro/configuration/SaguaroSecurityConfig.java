2c8c4f77f9c25347dc694f351abe66b1b0ea3595
charleswong739 <charleswonghm@gmail.com>
2021-10-23 22:46:39 -0400

    Enable h2 console through security

--- saguaro/src/main/java/com/saguaro/configuration/SaguaroSecurityConfig.java
+++ saguaro/src/main/java/com/saguaro/configuration/SaguaroSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +20,16 @@ public class SaguaroSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder builder) throws Exception{
         builder.authenticationProvider(authProvider(encoder()));
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll();
+
+        http.headers().frameOptions().disable(); // needed for h2 console
     }
 
     @Bean
