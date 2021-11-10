85fa2dfd9b161e93143f56214b15ebb0a174548e
rk4life2018 <rk4life2018@gmail.com>
2021-11-10 18:51:40 -0500

    Updated and added documentation to the AuthAdapter Interface

--- cereus/src/main/java/com/cactus/adapters/AuthAdapter.java
+++ cereus/src/main/java/com/cactus/adapters/AuthAdapter.java
@@ -8,8 +8,7 @@ import java.net.URISyntaxException;
 public interface AuthAdapter {
 
     /**
-     * Returns a Response object with the results of a login operation done using the
-     * provided username and password.
+     * Returns a User object that corresponds to the provided username and password.
      *
      * If the login was successful, the Response will have code 200 (OK), and a
      * payload with the following entries:
@@ -18,39 +17,46 @@ public interface AuthAdapter {
      *
      * The type in parentheses denotes the value's original type.
      *
-     * If the login was unsuccessful, the Response will have code 400 (BAD_REQUEST)
-     * and a null payload.
+     * If the username and password do not correspond to an existing User,
+     * then a User object is returned with null values for its attributes, and no login takes place
      *
      * @param username a String containing the username of the user to be logged in
      * @param password a String containing the password to validate
-     * @return         a Response to the login operation
+     * @return         a User object that corresponds to the given parameters
      *
-     * @see Response
+     * @see User
      */
     User login(String username, String password) throws IOException, InterruptedException, URISyntaxException;
 
     /**
-     * Returns a Response object with the results of a create user operation done
-     * using the provided username, password, and name.
+     * Returns a User object created with the provided username, password, and name.
      *
-     * If the user was created successfully, the Response will have code 200 (OK),
-     * and a payload with the following keys:
-     * - userid: a string representation of the user' ID (long)
-     * - name: a string representation of the user's name (string)
+     * Note: the resulting user object is also logged in as the current user, and no further call to login
+     * is required
      *
-     * The type in parentheses denotes the value's original type.
+     * If the user has the same username and password (but different name)
+     * as one that already exists, then that person is logged in and no creation takes place.
      *
-     * If the user creation was unsuccessful (i.e. username already exists), the
-     * Response will have code 400 (BAD_REQUEST) and a null payload.
+     * If the user has the same username and different password as an existing user,
+     * then a User object is returned with null values for its attributes, and no creation or login takes place
      *
      * @param username a String containing the username of the new user
      * @param password a String containing the password of the new user
      * @param name     a String containing the name of the user
-     * @return         a Response to the create user operation
+     * @return         a User object created with the given parameters
      *
-     * @see Response
+     * @see User
      */
     User create(String username, String password, String name) throws IOException, URISyntaxException, InterruptedException;
 
+    /**
+     * Returns whether the User object with the corresponding token is successfully logged out of the application.
+     *
+     *
+     * @param token    a String containing a token unique to every User, stored in the User class
+     * @return         whether the User corresponding to the token is logged out or not
+     *
+     * @see User
+     */
     boolean logout(String token) throws IOException, InterruptedException, URISyntaxException;
 }
