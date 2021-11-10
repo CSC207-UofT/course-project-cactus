642399cea1522c642bd0c4623158215f91d6ad98
dorsa-molaverdikhani <dorsa.molaverdikhani@mail.utoronto.ca>
2021-11-10 13:21:27 -0500

    Developed android part of the project for creating a grocery list.

--- cereus/src/main/java/com/cactus/ui/CreatingGroceryListActivity.java
+++ cereus/src/main/java/com/cactus/ui/CreatingGroceryListActivity.java
@@ -1,13 +1,38 @@
-package com.cactus.ui;
+package com.example.ui;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CreatingGroceryListActivity extends AppCompatActivity {
 
+    private EditText groceryListName;
+    private Button addListButton;
+    //private UserInteractFacade userFacade;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creating_grocery_list);
+
+        groceryListName = findViewById(R.id.groceryListName);
+        addListButton = findViewById(R.id.addListNameButton);
+        //userFacade = getIntent().getSerializableExtra("Facade");
+
+        addListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String givenGroceryListName = groceryListName.getText().toString();
+                //userFacade.newGroceryList(givenGroceryListName);
+                Intent intent = new Intent(CreatingGroceryListActivity.this, AddItemsActivity.class);
+                //intent.putExtra("Facade", userFacade);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- cereus/src/main/java/com/cactus/ui/GroceryLists.java
+++ cereus/src/main/java/com/cactus/ui/GroceryLists.java
@@ -9,5 +9,6 @@ public class GroceryLists extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery_lists);
+
     }
 }
