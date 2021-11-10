2a9535e341cfb489b01dba01350f395dbc20a9e7
rk4life2018 <rk4life2018@gmail.com>
2021-11-10 18:35:20 -0500

    Refactored common code in create and login methods to create sendRequest method and renamed variables to improve clarity.

--- cereus/src/main/java/com/cactus/adapters/AuthLogin.java
+++ cereus/src/main/java/com/cactus/adapters/AuthLogin.java
@@ -3,7 +3,6 @@ package com.cactus.adapters;
 import com.cactus.entities.User;
 
 import org.apache.http.client.utils.URIBuilder;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -11,7 +10,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
-import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.HashMap;
@@ -25,42 +23,59 @@ public class AuthLogin implements  AuthAdapter{
 
         ObjectMapper objectMapper = new ObjectMapper();
         String requestBody = objectMapper.writeValueAsString(login);
+
         HttpClient client = HttpClient.newHttpClient();
         URI uri = new URIBuilder("http://localhost:8080/login").addParameter("username",
                 username).addParameter("password", password).build();
+        RequestResponse result = sendRequest(login, uri);
+        return result.getFinalMapper().readValue(result.getResponse().body(), User.class);
+
+    }
+
+    @Override
+    public User create(String username, String password, String name) throws IOException, URISyntaxException, InterruptedException {
+        HashMap<String, String> create = new HashMap<>();
+        create.put("username", username);
+        create.put("password", password);
+        create.put("name", name);
+        URI uri = new URI("http://localhost:8080/register");
+        sendRequest(create, uri);
+        return login(username, password);
+
+    }
+
+    private RequestResponse sendRequest(HashMap<String, String> body, URI uri) throws IOException, InterruptedException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(body);
+        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(uri)
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(requestBody))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        ObjectMapper finalMapper = new
-                ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return finalMapper.readValue(response.body(), User.class);
-
+        ObjectMapper finalMapper =
+                new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return new RequestResponse(finalMapper, response);
     }
 
-    @Override
-    public User create(String username, String password, String name) throws IOException, URISyntaxException, InterruptedException {
-        HashMap<String, String> login = new HashMap<>();
-        login.put("username", username);
-        login.put("password", password);
-        login.put("name", name);
+    private class RequestResponse {
+        private final ObjectMapper finalMapper;
+        private final HttpResponse<String> response;
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = objectMapper.writeValueAsString(login);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("http://localhost:8080/register"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        ObjectMapper finalMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return login(username, password);
+        public RequestResponse(ObjectMapper finalMapper, HttpResponse<String> response) {
+            this.finalMapper = finalMapper;
+            this.response = response;
+        }
 
+        public ObjectMapper getFinalMapper() {
+            return finalMapper;
+        }
+
+        public HttpResponse<String> getResponse() {
+            return response;
+        }
     }
-
     @Override
     public boolean logout(String token) throws IOException, InterruptedException, URISyntaxException {
         HttpClient client = HttpClient.newHttpClient();
