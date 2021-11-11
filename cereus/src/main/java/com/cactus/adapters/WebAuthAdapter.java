9e35ebd80b8fb610ce5ac623fa85d550b0c69bf5
calebxcaleb <calebxcaleb@gmail.com>
2021-11-10 21:17:13 -0500

    added testing things

--- cereus/src/main/java/com/cactus/adapters/WebAuthAdapter.java
+++ cereus/src/main/java/com/cactus/adapters/WebAuthAdapter.java
@@ -5,16 +5,16 @@ import com.cactus.entities.User;
 public class WebAuthAdapter implements AuthAdapter {
     @Override
     public User login(String username, String password) {
-        return null;
+        return new User("Name", username, password);
     }
 
     @Override
     public User create(String username, String password, String name) {
-        return null;
+        return new User(name, username, password);
     }
 
     @Override
     public User getUser(long userId) {
-        return null;
+        return new User("Caleb", "calebxcaleb", "password");
     }
 }
