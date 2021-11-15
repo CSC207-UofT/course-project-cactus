67d184b8252399cddce34c530bc073c50998ce3f
charleswong739 <charleswonghm@gmail.com>
2021-11-14 22:48:47 -0500

    Clean up Cereus entities

--- cereus/src/main/java/com/cactus/entities/GroceryItem.java
+++ cereus/src/main/java/com/cactus/entities/GroceryItem.java
@@ -2,23 +2,12 @@
  * This file defines a GroceryItem class.
  */
 package com.cactus.entities;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Grocery Item Entity
  */
 public class GroceryItem {
 
     private String name;
-    private final ArrayList<Integer> userIdList;
-    private final long groceryListId;
-    private final Map<Integer, Integer> userMentions;
-
-    private long id;
-
 
     /**
      * Creates a new GroceryItem with the given name and id number and
@@ -26,45 +15,16 @@ public class GroceryItem {
      *
      * @param name A String containing the User's name.
      */
-    public GroceryItem(String name, long groceryListId) {
+    public GroceryItem(String name) {
         this.name = name;
-        this.userIdList = new ArrayList<>();
-        this.groceryListId = groceryListId;
-        this.userMentions = new HashMap<>();
-
-        this.id = 0L;
     }
 
     public String getName() {
         return name;
     }
 
-    public ArrayList<Integer> getUserIdList() {
-        return userIdList;
-    }
-
-    public long getGroceryListId() {
-        return groceryListId;
-    }
-
-    public Map<Integer, Integer> getUserMentions() {
-        return userMentions;
-    }
-
-    public long getId() {
-        return this.id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void addUserId(int userId) {
-        userIdList.add(userId);
     }
 
 }
--- cereus/src/main/java/com/cactus/entities/User.java
+++ cereus/src/main/java/com/cactus/entities/User.java
@@ -3,11 +3,6 @@
  */
 package com.cactus.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.sql.Timestamp;
-
 /**
  * User Entity
  */
@@ -22,14 +17,13 @@ public class User {
     /**
      * Creates a new User with the given name, username, password and id number.
      *
-     * @param name A String containing the User's name.
+     * @param name     A String containing the User's name.
      * @param username A String containing the User's username.
      * @param password A String containing the User's password.
-     * @param token A String containing a token to signify User was successfully retrieved from server
-     * @param id A long value serving as an identifier for the User.
+     * @param token    A String containing a token to signify User was successfully retrieved from server
+     * @param id       A long value serving as an identifier for the User.
      */
-
-    public User(String name, String username, String password, String token, long id){
+    public User(String name, String username, String password, String token, long id) {
         this.name = name;
         this.username = username;
         this.password = password;
@@ -37,37 +31,48 @@ public class User {
         this.id = id;
     }
 
-    public User(){
+    /**
+     * Empty constructor for User, required for Jackson serialization
+     */
+    public User() {}
 
-    }
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public String getUsername(){
+
+    public String getUsername() {
         return username;
     }
-    public String getPassword(){
+
+    public String getPassword() {
         return password;
     }
-    public String getToken(){
+
+    public String getToken() {
         return token;
     }
-    public long getId(){
+
+    public long getId() {
         return id;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    public void setUsername(String username){
+
+    public void setUsername(String username) {
         this.username = username;
     }
-    public void setToken(String token){
+
+    public void setToken(String token) {
         this.token = token;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
-    public void setId(long id){
+
+    public void setId(long id) {
         this.id = id;
     }
 
