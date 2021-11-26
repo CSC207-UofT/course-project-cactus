552d7dcb7bf10b2ef96ba4a94a97695dcff7f25d
charleswong739 <charleswonghm@gmail.com>
2021-11-26 12:53:40 -0500

    Add additional constructor for 404 exception

--- saguaro/src/main/java/com/saguaro/exception/ResourceNotFoundException.java
+++ saguaro/src/main/java/com/saguaro/exception/ResourceNotFoundException.java
@@ -1,26 +1,22 @@
 package com.saguaro.exception;
 
-import com.saguaro.entity.GroceryItem;
-import com.saguaro.entity.GroceryList;
 import com.saguaro.entity.User;
 
 public class ResourceNotFoundException extends Exception {
 
+    public ResourceNotFoundException(Class<?> type, String id) {
+        super(constructMessage(type, id));
+    }
+
     public ResourceNotFoundException(Class<?> type, String id, User user) {
-        super(constructMessage(type, id, user));
+        super(constructMessage(type, id));
     }
 
     private static String constructMessage(Class<?> type, String id, User user) {
-        String messageType;
+        return "Could not find " + type.getSimpleName() + " " + id + " for user " + user.getUsername();
+    }
 
-        if (type.equals(GroceryItem.class)) {
-            messageType = "grocery item";
-        } else if (type.equals(GroceryList.class)) {
-            messageType = "grocery list";
-        } else {
-            messageType = "resource";
-        }
-
-        return "Could not find " + messageType + " " + id + " for user " + user.getUsername();
+    private static String constructMessage(Class<?> type, String id) {
+        return "Could not find " + type.getSimpleName() + " " + id;
     }
 }
