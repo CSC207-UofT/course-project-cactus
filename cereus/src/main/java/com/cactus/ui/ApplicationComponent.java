3d389648fb77f2b20e1c19bcacc6a2ab97e8a472
Charles Wong <charleswonghm@gmail.com>
2021-12-04 04:05:12 -0500

    Clean up unneeded injections

--- cereus/src/main/java/com/cactus/ui/ApplicationComponent.java
+++ cereus/src/main/java/com/cactus/ui/ApplicationComponent.java
@@ -9,23 +9,8 @@ import javax.inject.Singleton;
         AppModule.class
 })
 public interface ApplicationComponent {
-    void inject(MainActivity mainActivity);
 
     void inject(AbstractActivity abstractActivity);
 
-    void inject(SignupActivity signupActivity);
-
-    void inject(DisplayingListsActivity displayingListsActivity);
-
-    void inject(DisplayingItemsActivity displayingItemsActivity);
-
-    void inject(CreateListActivity createListActivity);
-
-    void inject(CreateTemplateActivity createTemplateActivity);
-
     void inject(CustomAdapter customAdapter);
-
-    void inject (UserProfileActivity userProfileActivity);
-
-    void inject (AddFriendActivity addFriendActivity);
 }
