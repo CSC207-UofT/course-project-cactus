9c080efc79c12028c78433ead884547a91994e26
Charles Wong <charleswonghm@gmail.com>
2021-12-04 01:26:48 -0500

    Style user profile page

--- cereus/src/main/java/com/cactus/ui/UserProfileActivity.java
+++ cereus/src/main/java/com/cactus/ui/UserProfileActivity.java
@@ -1,69 +1,77 @@
 package com.cactus.ui;
 
 import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelStore;
 import com.cactus.systems.UserInteractFacade;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
 
-public class UserProfileActivity extends AppCompatActivity {
-
-    @Inject
-    UserInteractFacade userInteractFacade;
+public class UserProfileActivity extends AbstractActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        ((CereusApplication) getApplicationContext()).appComponent.inject(this);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile);
-        setTitle(userInteractFacade.getUserName() + "Profile");
-
-        displayOptions();
+    AbstractActivity activity() {
+        return this;
     }
 
-    private void displayOptions(){
-        EditText name = findViewById(R.id.currentName);
-        EditText password = findViewById(R.id.changedPassword);
-        Button logoutButton = findViewById(R.id.logoutButton);
-        Button addFriendButton = findViewById(R.id.addFriendButton);
-        Button changeButton = findViewById(R.id.changeButton);
-        name.setHint(userInteractFacade.getName());
-        //TODO: Add a getName method to UserInteractFacade that returns the user's name.
-        logoutButton.setOnClickListener(view -> {
-            if (userInteractFacade.logout()) {
-                Intent intent = new Intent(UserProfileActivity.this, MainActivity.class);
-                startActivity(intent);
-            } else{
-                Toast.makeText(UserProfileActivity.this, "Logout unsuccessful", Toast.LENGTH_LONG).show();
-            }
-        });
+    @Override
+    void activitySetup() {
+        setContentView(R.layout.activity_user_profile);
+//        View view = findViewById(R.id.cereusToolBar);
 
-        addFriendButton.setOnClickListener(view -> {
+        Toolbar toolbar = findViewById(R.id.cereusToolBar);
+        Button userButton = toolbar.findViewById(R.id.user_button);
+        userButton.setVisibility(View.INVISIBLE);
+    }
 
-            Intent intent = new Intent(UserProfileActivity.this, AddFriendActivity.class);
-            startActivity(intent);
+    protected void displayOptions(){
+//        EditText name = findViewById(R.id.currentName);
+//        EditText password = findViewById(R.id.changedPassword);
+//        Button logoutButton = findViewById(R.id.logoutButton);
+//        Button addFriendButton = findViewById(R.id.addFriendButton);
+//        Button changeButton = findViewById(R.id.changeButton);
+////        name.setHint(userInteractFacade.getName());
+//        //TODO: Add a getName method to UserInteractFacade that returns the user's name.
+//        logoutButton.setOnClickListener(view -> {
+//            if (userInteractFacade.logout()) {
+//                Intent intent = new Intent(UserProfileActivity.this, MainActivity.class);
+//                startActivity(intent);
+//            } else{
+//                Toast.makeText(UserProfileActivity.this, "Logout unsuccessful", Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//        addFriendButton.setOnClickListener(view -> {
+//
+//            Intent intent = new Intent(UserProfileActivity.this, AddFriendActivity.class);
+//            startActivity(intent);
+//
+//        });
 
-        });
-
-        changeButton.setOnClickListener(view -> {
-            String newName = name.getText().toString();
-            String newPassword = password.getText().toString();
-            if ((!newName.equals("")) & (!newPassword.equals(""))) {
-            //TODO: create a changeName and changePassword methods in the userInteractFacade
-                userInteractFacade.changeName(newName);
-                userInteractFacade.changePassword(newPassword);
-            } else if (!newName.equals("")){
-                userInteractFacade.changeName(newName);
-            } else if (!newPassword.equals("")) {
-                userInteractFacade.changePassword(newPassword);
-            }else{
-                Toast.makeText(UserProfileActivity.this, "Enter new username or password", Toast.LENGTH_LONG).show();
-            }
-        });
+//        changeButton.setOnClickListener(view -> {
+//            String newName = name.getText().toString();
+//            String newPassword = password.getText().toString();
+//            if ((!newName.equals("")) & (!newPassword.equals(""))) {
+//            //TODO: create a changeName and changePassword methods in the userInteractFacade
+//                userInteractFacade.changeName(newName);
+//                userInteractFacade.changePassword(newPassword);
+//            } else if (!newName.equals("")){
+//                userInteractFacade.changeName(newName);
+//            } else if (!newPassword.equals("")) {
+//                userInteractFacade.changePassword(newPassword);
+//            }else{
+//                Toast.makeText(UserProfileActivity.this, "Enter new username or password", Toast.LENGTH_LONG).show();
+//            }
+//        });
 
     }
 }
