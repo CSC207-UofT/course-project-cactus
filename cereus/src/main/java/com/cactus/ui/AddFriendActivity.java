9d809f2340246e98a99720bd02322909a66c737e
dorsa-molaverdikhani <dorsa.molaverdikhani@mail.utoronto.ca>
2021-12-04 09:29:43 -0500

    Completed AddFriendActivity.

--- cereus/src/main/java/com/cactus/ui/AddFriendActivity.java
+++ cereus/src/main/java/com/cactus/ui/AddFriendActivity.java
@@ -1,37 +1,55 @@
 package com.cactus.ui;
 
-import android.content.Intent;
-import android.widget.Button;
-import android.widget.EditText;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import com.cactus.systems.UserInteractFacade;
 
-import javax.inject.Inject;
+import android.widget.*;
+import java.util.ArrayList;
 
-public class AddFriendActivity extends AppCompatActivity {
+/***
+ * Represents the activity responsible for adding and displaying friends
+ */
 
-    @Inject
-    UserInteractFacade userInteractFacade;
+public class AddFriendActivity extends AbstractActivity {
+
+    private ArrayList<String> friends;
+    private ListView listView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_friend);
-
-        setTitle("Add Friend");
-
-        displayOptions();
+    protected AbstractActivity activity(){
+        return this;
     }
 
-    private void displayOptions(){
+    @Override
+    protected void activitySetup(){
+        setContentView(R.layout.activity_add_friend);
+
+        setTitle(this.userInteractFacade.getUserName());
+
+        //TODO: Add a getFriendList to UserInteractFacade
+        friends = userInteractFacade.getFriendsList();
+        CustomFriendListAdapter customFriendListAdapter = new CustomFriendListAdapter(this, R.layout.friend_list_layout, friends, ((CereusApplication) getApplicationContext()).appComponent);
+        listView = findViewById(R.id.friendViewDisplayItem);
+        listView.setAdapter(customFriendListAdapter);
+    }
+
+    @Override
+    protected void displayOptions(){
         EditText friendUsername = findViewById(R.id.friendUsername);
-        Button addFriendButton = findViewById(R.id.addButton);
+        Button addButton = findViewById(R.id.addButton);
 
-        addFriendButton.setOnClickListener(view -> {
-            //TODO: Add "addFriend" method to UserInteractFacade so you will be able to handle this case.
+        addButton.setOnClickListener(view -> {
+            String givenUsername = friendUsername.getText().toString();
+
+            //TODO: Add addFriend method to UserInteractFacade
+            if (!friends.contains(givenUsername) & this.userInteractFacade.addFriend(givenUsername)) {
+                friends.add(givenUsername);
+                ((BaseAdapter) listView.getAdapter()).notifyDataSetChanged();
+                friendUsername.getText().clear();
+            } else if (!this.userInteractFacade.addFriend(givenUsername)) {
+                Toast.makeText(AddFriendActivity.this, "Username not found", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(AddFriendActivity.this, "That friend is already added", Toast.LENGTH_LONG).show();
+            }
         });
-
     }
 
 }
