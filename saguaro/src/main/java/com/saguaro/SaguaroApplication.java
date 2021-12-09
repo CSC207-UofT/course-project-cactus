1a47b8c91faa4d484412fd489e5fa587df92a8f8
Charles Wong <charleswonghm@gmail.com>
2021-12-08 22:41:26 -0500

    Add documentation for main class

--- saguaro/src/main/java/com/saguaro/SaguaroApplication.java
+++ saguaro/src/main/java/com/saguaro/SaguaroApplication.java
@@ -3,9 +3,19 @@ package com.saguaro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class for Saguaro
+ *
+ * @author Charles Wong
+ */
 @SpringBootApplication
 public class SaguaroApplication {
 
+    /**
+     * Main method for Saguaro. Runs this Spring application with provided arguments.
+     *
+     * @param args an array of String command line arguments passed when running this application
+     */
     public static void main(String[] args) {
         SpringApplication.run(SaguaroApplication.class, args);
     }
